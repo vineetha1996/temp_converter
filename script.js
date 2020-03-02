f62bfec9ea2fb51e8e128664789f06cc026319e8
Vineetha Yenugula <vineethay96@gmail.com>
2020-03-02 10:35:08 -0600

    added changes in javaScript

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function convoCel(tempvalue) {
-	if (tempvalue == "") {} else {
-	tempvalue = parseFloat(tempvalue);
-	document.getElementById("outputCel").innerHTML=(tempvalue).toFixed(2) + ' &degC';
-	document.getElementById("outputFahr").innerHTML=((tempvalue*(9/5)+32)).toFixed(2) + ' &degF';
-	document.getElementById("outputKel").innerHTML=((tempvalue+273.15)).toFixed(2) + ' K';
-	document.getElementById('inputFahr').value = "";
-	document.getElementById('inputKel').value = "";
-	}
-}
 
-function convoFahr(tempvalue) {
-	if (tempvalue == "") {} else {
-	tempvalue = parseFloat(tempvalue);
-	document.getElementById("outputFahr").innerHTML=(tempvalue).toFixed(2) + ' &degF';
-	document.getElementById("outputCel").innerHTML=((tempvalue-32)/1.8).toFixed(2) + ' &degC';
-	document.getElementById("outputKel").innerHTML=((tempvalue+459.67)*(5/9)).toFixed(2) + ' K';
-	document.getElementById('inputCel').value = "";
-	document.getElementById('inputKel').value = "";
-	}
-}
 
 function convoKel(tempvalue) {
 	if (tempvalue == "") {} else {
