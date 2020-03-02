861d7eae8578ae0621aa93e555be575e075e4382
Vineetha Yenugula <vineethay96@gmail.com>
2020-03-02 10:41:04 -0600

    deleted few inputs

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ function convoKel(tempvalue) {
 	if (tempvalue == "") {} else {
 	tempvalue = parseFloat(tempvalue);
 	document.getElementById("outputKel").innerHTML=(tempvalue).toFixed(2) + ' K';
-	document.getElementById("outputCel").innerHTML=((tempvalue-273.15)).toFixed(2) + ' &degC';
-	document.getElementById("outputFahr").innerHTML=((tempvalue*(9/5)-459.67)).toFixed(2) + ' &degF';
 	document.getElementById('inputCel').value = "";
-	document.getElementById('inputFahr').value = "";
+	
 	}
 }
