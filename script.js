8eb18c1adb843c15149ef01c4d9aca7efcc5c10a
Vineetha Yenugula <vineethay96@gmail.com>
2020-03-02 11:22:59 -0600

    changed formula

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function convoKel(tempvalue) {
 	if (tempvalue == "") {} else {
 	tempvalue = parseFloat(tempvalue);
-	document.getElementById("outputKel").innerHTML=(tempvalue+283.5) + ' K';
+	document.getElementById("outputKel").innerHTML=(tempvalue+273.15) + ' K';
 	document.getElementById('inputCel').value = "";
 	
 	}
